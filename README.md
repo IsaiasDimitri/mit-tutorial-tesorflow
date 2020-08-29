19eef4c018abb20aeef741f382afc0ff9a2b68ef
IsaiasDimitri <isaiasdimitri2702@gmail.com>
2020-08-29 18:24:26 -0300

    Send to Github

---
+++ README.md
@@ -0,0 +1,42 @@
+### MIT Deep Learning Tutorial
+
+#### Topics this tutorial wants show
+
+- Feed Forward Neural Networks (FFNN's)
+
+- Convolutional Neural Networks (CNN's)
+
+#### My enviromment:
+
+- Ubuntu 20.04
+
+- Python 3.8
+
+To run this tutorial, first  download the repo:
+
+```bash
+git clone <repo>
+cd <repo>
+```
+
+Them, follow the steps to make a virtual environment and install our dependencies.
+
+```bash
+python3 -m venv .env
+source .env/bin/activate
+pip install -r requirements.txt
+```
+
+Now, you can run the part_1 and part_2, and see the output on terminal.
+
+```bash
+# note that inside venv, we call 'python', not 'python3'
+python part_1.py 
+python part_2.py 
+```
+
+
+
+Any doubts, you can contact me and I'll try help as faster  as possible.
+
+This README.md was written in [Typora](https://typora.io/).
---
+++ part_1.py
@@ -0,0 +1,70 @@
+# Boston housing price prediction
+# with FFNN's
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Dense
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+(train_features, train_labels), (test_features,
+                                 test_labels) = keras.datasets.boston_housing.load_data()
+train_mean = np.mean(train_features, axis=0)
+train_std = np.std(train_features, axis=0)
+train_features = (train_features - train_mean) / train_std
+
+
+def build_model():
+    model = keras.Sequential([
+        Dense(20, activation=tf.nn.relu,
+              input_shape=[len(train_features[0])]),
+        Dense(1)
+    ])
+
+    model.compile(optimizer=tf.optimizers.Adam(),
+                  loss='mse',
+                  metrics=['mae', 'mse'])
+    return model
+
+
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0:
+            print('')
+        print('.', end='')
+
+
+model = build_model()
+
+
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+history = model.fit(train_features, train_labels, epochs=1000, verbose=0,
+                    validation_split=0.1, callbacks=[early_stop, PrintDot()])
+
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+
+
+def plot_history():
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Square Error [Thousand Dollars$^2$]')
+    plt.plot(hist['epoch'], hist['mse'], label='Train Error')
+    plt.plot(hist['epoch'], hist['val_mse'], label='Val Error')
+    plt.legend()
+    plt.ylim([0, 50])
+    plt.show()
+
+
+rmse_final = np.sqrt(float(hist['val_mse'].tail(1)))
+print('\nFinal Root Mean Swaure Error on validation set: {}'.format(
+    round(rmse_final, 3)))
+
+
+# test_features_norm = (test_features - train_mean) / train_std
+# mse, _, _ = model.evaluate(test_features_norm, test_labels)
+# rmse = np.sqrt(mse)
+# print('Root Mean Square Error on test set: {}'.format(round(rmse, 3)))
+
+plot_history()
